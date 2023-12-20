7d4739de2fc5b83bab2757f84419efa563f450b5
Sergio Mattei <sergiomattei@outlook.com>
2023-12-20 11:27:36 -0400

    fix syntax error

--- modules/system.nix
+++ modules/system.nix
@@ -11,7 +11,7 @@
 {
 
   system = {
-    activationScripts are executed every time you boot the system or run `nixos-rebuild` / `darwin-rebuild`.
+    # activationScripts are executed every time you boot the system or run `nixos-rebuild` / `darwin-rebuild`.
     activationScripts.postUserActivation.text = ''
       # activateSettings -u will reload the settings from the database and apply them to the current session,
       # so we do not need to logout and login again to make the changes take effect.
