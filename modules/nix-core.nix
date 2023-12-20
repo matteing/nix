a59285b0e10075c4c4e95f4c6979ea04610ab7c1
Sergio Mattei <sergiomattei@outlook.com>
2023-12-20 12:23:08 -0400

    test

--- modules/nix-core.nix
+++ modules/nix-core.nix
@@ -9,4 +9,8 @@
   services.nix-daemon.enable = true;
   nix.package = pkgs.nix;
   programs.nix-index.enable = true;
+  system.configurationRevision = self.rev or self.dirtyRev or null;
+  system.stateVersion = 4;
+  nixpkgs.hostPlatform = "aarch64-darwin";
+  
 }
