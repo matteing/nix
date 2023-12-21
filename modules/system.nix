853f2b43d18ac6838dd818147f77ed70576f0306
Sergio Mattei <sergiomattei@outlook.com>
2023-12-21 01:20:53 -0400

    [checkin] 2023-12-21 01:20:53

--- modules/system.nix
+++ modules/system.nix
@@ -15,6 +15,8 @@
       # activateSettings -u will reload the settings from the database and apply them to the current session,
       # so we do not need to logout and login again to make the changes take effect.
       /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
+
+      osascript -e "tell application \"System Events\" to tell every desktop to set picture to \"${inputs.self}/wallpapers/orange-wave.jpg\" as POSIX file"
     '';
 
     defaults = {
