3a4e1be65148896ce80ebec8eb5a250700293356
Sergio Mattei <sergiomattei@outlook.com>
2023-12-20 23:22:55 -0400

    [checkin] 2023-12-20 23:22:55

--- modules/home.nix
+++ modules/home.nix
@@ -3,7 +3,7 @@
   # Home Manager needs a bit of information about you and the
   # paths it should manage.
   home.username = "sergio";
-  home.homeDirectory = "/home/sergio";
+  home.homeDirectory = "/Users/sergio";
 
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
