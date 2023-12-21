7843a95ec2c30ccdfb65141983c3bd7ef57e3cbb
Sergio Mattei <sergiomattei@outlook.com>
2023-12-20 23:14:41 -0400

    [checkin] 2023-12-20 23:14:41

--- flake.nix
+++ flake.nix
@@ -10,12 +10,6 @@
   outputs = inputs@{ self, nix-darwin, nixpkgs }:
   let
     configuration = { pkgs, ... }: {
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      environment.systemPackages =
-        [ pkgs.vim
-        ];
-
       # Auto upgrade nix package and the daemon service.
       services.nix-daemon.enable = true;
       # nix.package = pkgs.nix;
