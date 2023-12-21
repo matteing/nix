833e944a68e4f0132443f91560b0a7160951728e
Sergio Mattei <sergiomattei@outlook.com>
2023-12-21 01:54:54 -0400

    [checkin] 2023-12-21 01:54:54

--- modules/hostname.nix
+++ modules/hostname.nix
@@ -17,11 +17,10 @@ in
   system.defaults.smb.NetBIOSName = hostname;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
-  # I don't need this right now--I'm setting Darwin up for my personal account.
-  # users.users."${username}"= {
-  #   home = "/Users/${username}";
-  #   description = username;
-  # };
+  users.users."${username}"= {
+    home = "/Users/${username}";
+    description = username;
+  };
 
   nix.settings.trusted-users = [ username ];
 }
